0984d257fe3a637e8cba892ff99cccde749ebe23
Ben Sparks <bensparks0@gmail.com>
2021-02-22 10:18:09 +0200

    Added useDebounce from Code Collection

---
+++ useDebounce.js
@@ -0,0 +1,21 @@
+// From Code Collection
+// Throttles API calls on the client side to avoid spamming
+// a server
+
+import {useState, useEffect} from 'react'
+
+export const useDebounce = (value, delay) => {
+    const [debouncedValue, setDebouncedValue] = useState(value)
+
+    useEffect(() => {
+        const handler = setTimeout(() => {
+            setDebouncedValue(value)
+        }, delay)
+
+        return () => {
+            clearTimeout(handler)
+        }
+    }, [value])
+
+    return debouncedValue
+}
